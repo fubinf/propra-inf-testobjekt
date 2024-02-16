c1583b364bda914b478b41b9563d75c53a2a51e0
Ronny Ruhe <ronny.ruhe@cgi.com>
2024-02-16 08:38:25 +0100

    updated test class with parent path for import

--- v1.0.0/tests/unittests/app_tests.py
+++ v1.0.0/tests/unittests/app_tests.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
 import unittest
 from app import app, db, User
 from werkzeug.security import generate_password_hash
