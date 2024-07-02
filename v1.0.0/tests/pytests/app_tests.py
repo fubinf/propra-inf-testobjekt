84aac6249a0c01a74d2f6dcfdd6bb30209e0ecf2
Ronny Ruhe <ronnyruhe@icloud.com>
2024-07-02 21:21:11 +0200

    added os path

--- v1.0.0/tests/pytests/app_tests.py
+++ v1.0.0/tests/pytests/app_tests.py
@@ -1,5 +1,7 @@
+import os
 import sys
 sys.path.append("v1.0.0")
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 import pytest
 from app import app, db, User
